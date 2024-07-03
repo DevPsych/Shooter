a2abba9e6a16f046ad8dea9bc3bd34871e7c0905
DevPsych <140076838+DevPsych@users.noreply.github.com>
2024-07-03 20:51:12 +0900

    commit 16
    -when another body enters what a laser can collide with, the laser is destroyed using queue_free()
    -drones have an added take_damage() function that is checked for when it enters a laser's Area2D
    -lasers are destroyed from the game if it doesn't collide with anything after 2 seconds (game optimization)

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -7,3 +7,6 @@ func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Vector2.RIGHT
 	self.velocity = direction * SPEED
 	self.move_and_slide()
+	
+func take_damage():
+	print("damaged")
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,5 +3,16 @@ extends Area2D
 @export var speed: int = 1000
 var direction: Vector2 = Vector2.UP #This will be overridden by the player's direction gotten from signal
 
+func _ready():
+	$SelfDestructTimer.start()
+
 func _process(delta: float) -> void:
 	self.position += direction * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if "take_damage" in body:
+		body.take_damage()
+	self.queue_free()
+
+func _on_timer_timeout() -> void:
+	self.queue_free()
