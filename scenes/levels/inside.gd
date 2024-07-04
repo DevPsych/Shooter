8c513c18bbc4523ea94a3e2a8f54168a6d76bf01
DevPsych <140076838+DevPsych@users.noreply.github.com>
2024-07-04 23:09:19 +0900

    commit 19
    -added a hard transition to and from the Outside Level scene and the Inside Level scene.

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,4 +1,8 @@
 extends LevelParent
 
+# can't reload outside.tscn because it is the main scene?
+var outside_level_scene: PackedScene = load("res://scenes/levels/outside.tscn")
+
 func _on_exit_gate_area_body_entered(_body: Node2D) -> void:
 	self.tween_stop_player()
+	get_tree().change_scene_to_packed.bind(outside_level_scene).call_deferred()
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,7 +1,6 @@
 extends LevelParent
 
-func _on_gate_player_entered_gate(_body: Node2D) -> void:
-	self.tween_stop_player()
+@export var inside_level_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
 
 func _on_house_player_entered() -> void:
 	var tween = get_tree().create_tween()
@@ -10,3 +9,8 @@ func _on_house_player_entered() -> void:
 func _on_house_player_exited() -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property(%Camera2D,"zoom",Vector2(0.6,0.6),1)
+
+func _on_gate_player_entered_gate(_body: Node2D) -> void:
+	self.tween_stop_player()
+	get_tree().change_scene_to_packed.bind(inside_level_scene).call_deferred()
+	
