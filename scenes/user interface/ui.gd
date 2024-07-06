d9d0545d2ad06dcce8fed72769986411c0338b8f
DevPsych <140076838+DevPsych@users.noreply.github.com>
2024-07-06 12:02:01 +0900

    commit 22
    -added some modulation to the laser and grenade ammo UI. When the player has ammo, the amount of ammo and the ammo icon are green, but when the player has 0 ammo, then the label and icon turn red

--- scenes/user interface/ui.gd
+++ scenes/user interface/ui.gd
@@ -1,14 +1,32 @@
 extends CanvasLayer
 
+# colors
+var green: Color = Color("6bbfa3")
+var red: Color = Color(.9,0,0,1)
+
 @onready var laser_label: Label = $MarginContainer2/HBoxContainer/LaserCounter/LaserLabel
 @onready var grenade_label: Label = $MarginContainer2/HBoxContainer/GrenadeCounter/GrenadeLabel
+@onready var laser_icon: TextureRect = $MarginContainer2/HBoxContainer/LaserCounter/LaserIcon
+@onready var grenade_icon: TextureRect = $MarginContainer2/HBoxContainer/GrenadeCounter/GrenadeIcon
 
 func _ready():
 	update_laser_label()
 	update_grenade_label()
+	update_color(Globals.laser_amount, laser_label, laser_icon)
+	update_color(Globals.grenade_amount, grenade_label, grenade_icon)
 
 func update_laser_label():
 	laser_label.text = str(Globals.laser_amount)
+	update_color(Globals.laser_amount, laser_label, laser_icon)
 
 func update_grenade_label():
 	grenade_label.text = str(Globals.grenade_amount)
+	update_color(Globals.grenade_amount, grenade_label, grenade_icon)
+	
+func update_color(amount: int, label: Label, icon: TextureRect) -> void:
+	if amount > 0:
+		label.modulate = green
+		icon.modulate = green
+	else:
+		label.modulate = red
+		icon.modulate = red
