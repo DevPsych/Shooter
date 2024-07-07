944607149fd38e7f52e63645f7011ce2601ebf4e
DevPsych <140076838+DevPsych@users.noreply.github.com>
2024-07-07 11:48:23 +0900

    commit 28
    -added functionality to the grenade so if "entities" or "containers" are in the explosion radius (Area2D) then they will take damage over time

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -26,3 +26,6 @@ func _on_attack_area_body_exited(_body: Node2D) -> void:
 
 func _on_timer_timeout() -> void:
 	can_laser = true
+
+func take_damage():
+	print("Scout damaged")
--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -3,5 +3,21 @@ extends RigidBody2D
 @export var speed: int = 1000
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var entity_is_in_explosion: bool = false
+var entity: Node2D
+
 func explode():
 	animation_player.play("Explosion")
+	
+func _process(_delta):
+	if entity_is_in_explosion and "take_damage" in entity:
+		entity.take_damage()
+
+func _on_explosion_radius_body_entered(body: Node2D) -> void:
+	if body.is_in_group("entities") or body.is_in_group("containers"):
+		entity_is_in_explosion = true
+		entity = body
+
+func _on_explosion_radius_body_exited(body: Node2D) -> void:
+	if body.is_in_group("entities") or body.is_in_group("containers"):
+		entity_is_in_explosion = false
